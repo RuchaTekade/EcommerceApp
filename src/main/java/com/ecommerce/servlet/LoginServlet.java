db49f3a0c3975352d13242183421eb396e4d2303
Rucha Tekade <110525534+RuchaTekade@users.noreply.github.com>
2026-04-23 11:33:17 +0530

    Refactor LoginServlet for improved readability

--- src/main/java/com/ecommerce/servlet/LoginServlet.java
+++ src/main/java/com/ecommerce/servlet/LoginServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/login")
+
 public class LoginServlet extends HttpServlet {
     
     private static final String DEMO_USER = "demo";
