64b271a3c44347a0570167b98edaa45dcb4914ac
Rucha Tekade <110525534+RuchaTekade@users.noreply.github.com>
2026-04-23 10:39:56 +0530

    Update LoginServlet for demo mode
    Refactored login logic to use demo credentials for authentication.

--- src/main/java/com/ecommerce/servlet/LoginServlet.java
+++ src/main/java/com/ecommerce/servlet/LoginServlet.java
@@ -1,19 +1,18 @@
 package com.ecommerce.servlet;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import com.ecommerce.db.DBConnection;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+    
+    private static final String DEMO_USER = "demo";
+    private static final String DEMO_PASSWORD = "demo123";
     
     protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
@@ -21,30 +20,12 @@ public class LoginServlet extends HttpServlet {
         String name = request.getParameter("username");
         String password = request.getParameter("password");
         
-        try {
-            Connection conn = DBConnection.getConnection();
-            String query = "SELECT * FROM users WHERE name = ? AND password = ?";
-            PreparedStatement pstmt = conn.prepareStatement(query);
-            pstmt.setString(1, name);
-            pstmt.setString(2, password);
-            
-            ResultSet rs = pstmt.executeQuery();
-            
-            if (rs.next()) {
-                HttpSession session = request.getSession();
-                session.setAttribute("user_id", rs.getInt("id"));
-                session.setAttribute("user_name", rs.getString("name"));
-                response.sendRedirect("products.html");
-            } else {
-                response.sendRedirect("login.html?error=invalid");
-            }
-            
-            rs.close();
-            pstmt.close();
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-            response.sendRedirect("login.html?error=failed");
+        if (DEMO_USER.equals(name) && DEMO_PASSWORD.equals(password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user_name", name);
+            response.sendRedirect("products.html");
+        } else {
+            response.sendRedirect("login.html?error=invalid");
         }
     }
 }
